b9d4982a4517438299c54d001433f66d46132c0b
gustavo <gustavo@localhost>
2013-04-24 11:39:20 -0300

    Ok

--- themes/js/login.js
+++ themes/js/login.js
@@ -1,7 +1,46 @@
-$(document).on('pageinit', function() {
+var db = null;
+if (window.openDatabase) {
+    db = window.openDatabase("taxi", "", "TAXI", 5 * 1000 * 1000);
+    populateDB();
+} else {
+    jAviso("Navegador sem suporte ao banco de dados SqLite.");
+}
 
+function populateDB() {
+    db.transaction(function(e) {
+        e.executeSql('DROP TABLE IF EXISTS usuarios');
+        e.executeSql('CREATE TABLE IF NOT EXISTS usuarios ("id" INTEGER , "usuario" VARCHAR(50), "senha" VARCHAR(50))');
+        e.executeSql('INSERT INTO usuarios (id, usuario, senha) VALUES (1, "a" , "123")');
+    });
+}
+
+function errorCB(err) {
+    alert("Error processing SQL: " + err.code);
+}
+
+function successCB() {
+    alert("success!");
+}
+
+function validaLogin(usuario, senha) {
+    var c = 'SELECT * FROM usuarios WHERE usuario="' + usuario + '" AND senha="' + senha + '"';
+    var retorno = false;
+    db.transaction(function(e) {
+        e.executeSql(c, [],
+                function(g, f) {
+                    if (f.rows.length != 0) {
+                         _constant.redirect('menu.html');
+                    } else {
+                        alert("Informe login");
+                    }
+                });
+    });
+
+    return retorno;
+}
+
+$(document).on('pageinit', function() {
     $('.bt_logar').click(function(e) {
-        
         $('#dialogPage').popup();
         if ($('#usuario').val() == '') {
             $('#dialogPage').find('p').html('Informe o usuário');
@@ -12,17 +51,12 @@ $(document).on('pageinit', function() {
             $('#dialogPage').popup("open", {positionTo: '#senha'});
             $('#senha').focus();
         } else {
-            _constant.redirect('menu.html');
+            validaLogin($('#usuario').val(), $('#senha').val());
         }
     });
-
-    /*
-     $('#frm_login').submit(function(e) {
-     
-     e.preventDefault();
-     $.mobile.changePage('#dialogPage', {transition: "pop", role:"dialog"});
-     
-     //_constant.redirect('menu.html');       
-     });*/
-
+    //document.addEventListener("deviceready", onDeviceReady, false);    
+    //onDeviceReady();
 });
+
+
+

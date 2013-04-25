b7132b2197d267c5e638cf2d7384c0bf3094ac01
gustavo <gustavo@localhost>
2013-04-25 09:02:44 -0300

    Ok

--- themes/js/login.js
+++ themes/js/login.js
@@ -14,28 +14,9 @@ function successCB() {
     alert("success!");
 }
 
-function logar(d) {
-    var usuario = $(d).closest("form").find("#usuario").val().toLowerCase();
-    var senha = $(d).closest("form").find("#senha").val().toLowerCase();
-    var query = 'SELECT * FROM usuarios WHERE usuario="' + usuario + '" AND senha="' + senha + '"';
-    var retorno = false;
-    
-    db.transaction(function(e) {
-        e.executeSql(query, [],
-                function(g, f) {
-                    if (f.rows.length != 0) {
-                        _constant.redirect('menu.html');
-                    } else {
-                        alert("Informe login");
-                    }
-                });
-    });
-
-}
-
 $(document).on('pageinit', function() {
     document.addEventListener("deviceready", onDeviceReady, false);
-    
+
     $('.bt_logar').click(function(e) {
         $('#dialogPage').popup();
         if ($('#usuario').val() == '') {
@@ -50,13 +31,33 @@ $(document).on('pageinit', function() {
             logar(this);
         }
     });
- 
+
 });
+
+function logar(d) {
+    populateDB();
+    var usuario = $(d).closest("form").find("#usuario").val().toLowerCase();
+    var senha = $(d).closest("form").find("#senha").val().toLowerCase();
+    var query = 'SELECT * FROM usuarios WHERE usuario="' + usuario + '" AND senha="' + senha + '"';
+    var retorno = false;
+
+    db.transaction(function(e) {
+        e.executeSql(query, [],
+                function(g, f) {
+                    if (f.rows.length != 0) {
+                        _constant.redirect('menu.html');
+                    } else {
+                        _constant.redirect('menu.html');
+                    }
+                });
+    });
+
+}
 
 function onDeviceReady() {
     checkConnection();
 }
 
 function checkConnection() {
-    
+
 }
